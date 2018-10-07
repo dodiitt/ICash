1e80d58fb06aa53552bcd74cdbf5ea8511a7d110
= <levantuan.itvn@gmail.com>
2018-10-07 12:08:57 +0700

    initial commit

---
+++ src/main/java/com/icash/ICashApplication.java
@@ -0,0 +1,11 @@
+package com.icash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICashApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ICashApplication.class);
+    }
+}
---
+++ src/main/java/com/icash/utils/ApplicationInstant.java
@@ -0,0 +1,6 @@
+package com.icash.utils;
+
+public class ApplicationInstant {
+    public static final String HOME_URL = "/home";
+    public static final String HOME_HTML = "home";
+}
